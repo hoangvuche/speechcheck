6fdac49734559bb4ae38f6dc78ff3c1fde2077c4
hoangvuche <hoangvu.che@gmail.com>
2023-03-28 01:58:10 +0700

    initial commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sqlite3
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 
@@ -29,9 +30,28 @@ class RootWidget(FloatLayout):
 
     def __init__(self, **kwargs):
         super(RootWidget, self).__init__(**kwargs)
+        self.get_db_connection()
+        self.load_keywords()
 
     def load_keywords(self):
-        pass
+        cursor = self.conn.cursor()
+        sql = 'select * from keywords'
+        cursor.execute(sql)
+        record = cursor.fetchall()
+        for row in record:
+            print(row)
+        cursor.close()
+
+    def get_db_connection(self):
+        try:
+            self.conn = sqlite3.connect('qcdb.db')
+        except sqlite3.Error as error:
+            print('Error while connecting to sqlite')
+
+    def close_db_connection(self):
+        if self.conn:
+            self.conn.close()
+            print('DB connection closed')
 
 
 class SpeechQCApp(App):
